80004f1599a603ace301e54a7c2a67607a124cae
wjdals0627 <wjdals0627@gmail.com>
2025-03-25 11:46:44 +0900

    HI

---
+++ helloworld/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "신정민";
+        int age = 23;
+        System.out.println(name + "님은 " + age + "살 입니다.");
+    }
+}
---
+++ helloworld/src/practice.java
@@ -0,0 +1,11 @@
+public class practice {
+    public static void main(String[] args) {
+        final double PI = 3.14;
+        double radius = 10;
+
+        double area = (PI * radius * radius);
+
+        System.out.println("반지름이 "+ (int)radius + "인 원의 면적은 "+ (int)area + "이다.");
+
+    }
+}
