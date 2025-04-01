a423e4b358014e2392cabc1bd46e2cc1c7a42a4d
wjdals0627 <wjdals0627@gmail.com>
2025-04-01 11:54:59 +0900

    hihi

--- helloworld/src/InputNum1.java
+++ helloworld/src/InputNum1.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class InputNum1 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int num;
+        int num1, num2, num3;
 
         System.out.print("숫자를 입력하시오: ");
-        num = sc.nextInt();
 
-        System.out.println(num);
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+        num3 = sc.nextInt();
+
+        System.out.println(num1);
     }
 }
--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        String name = "신정민";
-        int age = 23;
-        System.out.println(name + "님은 " + age + "살 입니다.");
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        int num3;
+        System.out.print("숫자를 입력하시오: ");
+        num1 = sc.nextInt();
+        System.out.print("숫자를 입력하시오: ");
+        num2 = sc.nextInt();
+        System.out.print("숫자를 입력하시오: ");
+        num3 = sc.nextInt();
+        System.out.println(num1-num2-num3);
     }
 }
