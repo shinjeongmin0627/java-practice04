19e16bdb82724c0741e433f296f299548ccc3706
wjdals0627 <wjdals0627@gmail.com>
2025-04-01 10:39:25 +0900

    hi

---
+++ helloworld/src/InputNum1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class InputNum1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.print("숫자를 입력하시오: ");
+        num = sc.nextInt();
+
+        System.out.println(num);
+    }
+}
